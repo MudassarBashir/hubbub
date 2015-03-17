a2a2346c8c0d3d10bed6e78a78dbabfc20c5d329
Moe B <moeb59@gmail.com>
2015-03-16 22:57:48 -0400

    Created integration tests for the user class and added field constraints. Stopping at page 74 of book.

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -8,5 +8,8 @@ class User {
                      // provided by GORM.
 
     static constraints = {
+        loginId size: 3..20, unique: true, nullable: false
+        password size: 6..8, nullable: false
+        homepage url: true, nullable: true
     }
 }
