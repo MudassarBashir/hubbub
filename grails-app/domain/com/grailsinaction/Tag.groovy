e8ac16c8d584d45e674ffd3ae3fa321942d11dad
Moe B <moeb59@gmail.com>
2015-03-19 19:26:58 -0400

    Created the m:n (many to many) relationship between Post and Tag.

--- grails-app/domain/com/grailsinaction/Tag.groovy
+++ grails-app/domain/com/grailsinaction/Tag.groovy
@@ -2,6 +2,14 @@ package com.grailsinaction
 
 class Tag {
 
+    String name
+    User user
+
     static constraints = {
+        name blank: false
     }
+
+    static hasMany = [ posts : Post ]
+    static belongsTo = [ User, Post ]
+
 }
