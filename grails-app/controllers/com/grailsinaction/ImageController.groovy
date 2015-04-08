69e30e80f40bf8ac7fc75e6b27295bcd200616d0
Moe B <moeb59@gmail.com>
2015-04-08 18:15:25 -0400

    Added an upload method to the image controller. It will use a command object to upload photos.

--- grails-app/controllers/com/grailsinaction/ImageController.groovy
+++ grails-app/controllers/com/grailsinaction/ImageController.groovy
@@ -1,6 +1,17 @@
 package com.grailsinaction
 
+class PhotoUploadCommand {
+    byte[] photo
+    String loginId
+}
 class ImageController {
-
-    def index() { }
+    def upload(PhotoUploadCommand puc) {
+        def user = User.findByLoginId(puc.loginId)
+        user.profile.photo = puc.photo
+        redirect controller: "user", action: "profile", id: puc.loginId
+    }
+    def form() {
+// pass through to upload form
+        [ userList : User.list() ]
+    }
 }
