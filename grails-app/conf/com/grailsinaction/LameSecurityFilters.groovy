9c2d06b4835d6390f8a3b50b050a203d0ad89cde
Moe B <moeb59@gmail.com>
2015-04-08 20:29:45 -0400

    Section 7.5. Added a lame security filter.

---
+++ grails-app/conf/com/grailsinaction/LameSecurityFilters.groovy
@@ -0,0 +1,18 @@
+package com.grailsinaction
+
+class LameSecurityFilters {
+
+    def filters = {
+        all(controller:'*', action:'*') {
+            before = {
+
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+    }
+}
