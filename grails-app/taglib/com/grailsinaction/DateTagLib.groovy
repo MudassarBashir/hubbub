13736d98e86570b4245fe6864af3316dae194e93
Moe B <moeb59@gmail.com>
2015-04-21 18:05:26 -0400

    defined custom simple tag.

--- grails-app/taglib/com/grailsinaction/DateTagLib.groovy
+++ grails-app/taglib/com/grailsinaction/DateTagLib.groovy
@@ -18,27 +18,40 @@ class DateTagLib {
         def niceTime = ""
         long calc = 0;
         calc = Math.floor(diff / day)
-        if (calc) {
-            niceTime += calc + " day" + (calc > 1 ? "s " : " ")
-            diff %= day
+        if (diff >= minute) {
+            if (calc) {
+                niceTime += calc + " day" + (calc > 1 ? "s " : " ")
+                diff %= day
+            }
+            calc = Math.floor(diff / hour)
+            if (calc) {
+                niceTime += calc + " hour" + (calc > 1 ? "s " : " ")
+                diff %= hour
+            }
+            calc = Math.floor(diff / minute)
+            if (calc) {
+                niceTime += calc + " minute" + (calc > 1 ? "s " : " ")
+                diff %= minute
+            }
         }
-        calc = Math.floor(diff / hour)
-        if (calc) {
-            niceTime += calc + " hour" + (calc > 1 ? "s " : " ")
-            diff %= hour
+        else {
+            if (diff) {
+                def seconds = diff / second
+                niceTime = seconds + " second" + (seconds > 1 ? "s " : " ")
+            }
         }
-        calc = Math.floor(diff / minute)
-        if (calc) {
-            niceTime += calc + " minute" + (calc > 1 ? "s " : " ")
-            diff %= minute
-        }
+
         if (!niceTime) {
             niceTime = "Right now"
-        } else {
+        }
+        else {
             niceTime += (date.time > now.time) ? "from now" : "ago"
         }
+
         return niceTime
     }
+    static namespace = "hub"
+
     static defaultEncodeAs = 'html'
     //static encodeAsForTags = [tagName: 'raw']
 }
--- test/unit/com/grailsinaction/DateTagLibSpec.groovy
+++ test/unit/com/grailsinaction/DateTagLibSpec.groovy
@@ -1,7 +1,8 @@
 package com.grailsinaction
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import groovy.time.TimeCategory
+import spock.lang.*
 
 /**
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
@@ -9,12 +10,17 @@ import spock.lang.Specification
 @TestFor(DateTagLib)
 class DateTagLibSpec extends Specification {
 
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test something"() {
+    @Unroll
+    void "Conversion of #testName matches #expectedNiceDate"() {
+        expect:
+            applyTemplate('<hub:dateFromNow date="${date}" />',
+                    [date: testDate]) == expectedNiceDate
+        where:
+            testName | testDate | expectedNiceDate
+            "Current Time" | new Date() | "Right now"
+            "Now - 1 day" | new Date().minus(1) | "1 day ago"
+            "Now - 2 days" | new Date().minus(2) | "2 days ago"
+            "Now - 5 minutes" | use (TimeCategory) { 5.minutes.ago } | "5 minutes ago"
+            "Now - 55 seconds" | use (TimeCategory) { 55.seconds.ago } | "55 seconds ago"
     }
 }
