eafc77062355132e23d466246442cae1f58b066b
Moe B <moeb59@gmail.com>
2015-07-17 21:24:01 -0400

    Geb functional testing until listing 9.9

--- test/functional/com/grailsinaction/TimelineFunctionalSpec.groovy
+++ test/functional/com/grailsinaction/TimelineFunctionalSpec.groovy
@@ -15,4 +15,40 @@ class TimelineFunctionalSpec extends GebReportingSpec {
                 "What is Phil Potts hacking on right now?"
     }
 
+//    def "Submitting a new post"() {
+//        given: "I log in and start at my timeline page"
+//        login "frankie", "testing"
+//        go "users/phil"
+//        when: "I enter a new message and post it"
+//        $("#postContent").value("This is a test post from Geb")
+//        $("#newPost").find("input", type: "button").click()
+//        then: "I see the new post in the timeline"
+//        waitFor {
+//            $("div.postText", text: "This is a test post from Geb").present
+//        }
+//    }
+/*  This test is outdated as Peter Ledbrook said the present() function is no longer used in geb. So this functional
+test gets replaced by the one below it.
+ */
+
+    def "Submitting a new post"() {
+        given: "I log in and start at my timeline page"
+        login "frankie", "testing"
+        go "timeline"
+
+        when: "I enter a new message and post it"
+        $("#postContent").value("This is a test post from Geb")
+        $("#newPost").find("input", type: "button").click()
+
+        then: "I see the new post in the timeline"
+        waitFor { !$("div.postText", text: "This is a test post from Geb").empty }
+    }
+
+    private login(String username, String password) {
+        go "login/form"
+        $("input[name='loginId']").value(username)
+        $("input[name='password']").value(password)
+        $("input[type='submit']").click()
+    }
+
 }
